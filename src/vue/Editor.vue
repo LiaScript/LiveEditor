d4c12e69b20382fcad62a24295d9a2a7a254fc11
André Dietrich <andredietrich@web.de>
2022-10-04 11:30:00 +0200

    Add word-wrap

--- src/vue/Editor.vue
+++ src/vue/Editor.vue
@@ -38,6 +38,7 @@ export default {
       language: "markdown",
       theme: this.lights ? "vs-light" : "vs-dark",
       automaticLayout: true,
+      wordWrap: "on",
     });
 
     this.editor = editor;
