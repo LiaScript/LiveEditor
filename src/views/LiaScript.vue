44152316051b1511ba70291e32d2fd015e480572
André Dietrich <andredietrich@web.de>
2022-10-05 17:55:33 +0200

    add monaco worker

--- src/views/LiaScript.vue
+++ src/views/LiaScript.vue
@@ -138,6 +138,35 @@ import { tutorial } from "../ts/views/Helper/Tutorial";
 import Editor from "../components/Editor.vue";
 import Preview from "../components/Preview.vue";
 
+// @ts-ignore
+import JSONWorker from "url:monaco-editor/esm/vs/language/json/json.worker.js";
+// @ts-ignore
+import CSSWorker from "url:monaco-editor/esm/vs/language/css/css.worker.js";
+// @ts-ignore
+import HTMLWorker from "url:monaco-editor/esm/vs/language/html/html.worker.js";
+// @ts-ignore
+import TSWorker from "url:monaco-editor/esm/vs/language/typescript/ts.worker.js";
+// @ts-ignore
+import EditorWorker from "url:monaco-editor/esm/vs/editor/editor.worker.js";
+
+window.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    if (label === "json") {
+      return JSONWorker;
+    }
+    if (label === "css" || label === "scss" || label === "less") {
+      return CSSWorker;
+    }
+    if (label === "html" || label === "handlebars" || label === "razor") {
+      return HTMLWorker;
+    }
+    if (label === "typescript" || label === "javascript") {
+      return TSWorker;
+    }
+    return EditorWorker;
+  },
+};
+
 export default {
   name: "LiaScript",
 
