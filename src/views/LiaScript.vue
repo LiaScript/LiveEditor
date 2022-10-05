bc26b4e56ce2b7942c3de3d31562cfdae4ea36ea
André Dietrich <andredietrich@web.de>
2022-10-05 14:23:06 +0200

    Add view settings

--- src/views/LiaScript.vue
+++ src/views/LiaScript.vue
@@ -1,4 +1,75 @@
 <template>
+
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a
+        class="navbar-brand"
+        href="./"
+      >[Title]</a>
+
+      <div
+        class="btn-toolbar btn-group-sm"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div
+          class="btn-group btn-outline-secondary me-2"
+          role="group"
+          aria-label="Basic radio toggle button group"
+        >
+          <input
+            type="radio"
+            class="btn-check"
+            name="btnradio"
+            id="btnradio1"
+            autocomplete="off"
+            @click="changeMode(-1)"
+          >
+          <label
+            class="btn btn-outline-secondary"
+            for="btnradio1"
+            title="Editor only"
+          >
+            <i class="bi bi-pencil"></i>
+          </label>
+
+          <input
+            type="radio"
+            class="btn-check"
+            name="btnradio"
+            id="btnradio2"
+            autocomplete="off"
+            checked
+            @click="changeMode(0)"
+          >
+          <label
+            class="btn btn-outline-secondary"
+            for="btnradio2"
+            title="Split view"
+          >
+            <i class="bi bi-layout-split"></i>
+          </label>
+
+          <input
+            type="radio"
+            class="btn-check"
+            name="btnradio"
+            id="btnradio3"
+            autocomplete="off"
+            @click="changeMode(1)"
+          >
+          <label
+            class="btn btn-outline-secondary"
+            for="btnradio3"
+            title="Preview only"
+          >
+            <i class="bi bi-eye"></i>
+          </label>
+        </div>
+      </div>
+    </div>
+  </nav>
+
   <div
     class="container p-0"
     style="max-width:100%"
@@ -8,7 +79,11 @@
       class="row align-items-start p-0 m-0 flex-nowrap"
       style="height: calc(100vh - 56px);"
     >
-      <div class="col-6 w-50 p-0 h-100">
+      <div
+        class="col-6 w-50 p-0 h-100"
+        :hidden="mode > 0"
+        :class="{'w-50': mode==0, 'w-100': mode!=0}"
+      >
         <Editor
           class="col w-100 p-0 h-100"
           @compile="compile"
@@ -19,7 +94,11 @@
         </Editor>
       </div>
 
-      <div class="col-6 w-50 h-100 p-0">
+      <div
+        class="col-6 w-50 h-100 p-0"
+        :hidden="mode < 0"
+        :class="{'w-50': mode==0, 'w-100': mode!=0}"
+      >
         <div
           v-show="previewNotReady"
           style="position: absolute; background-color: white; width:50%; height: 100%"
@@ -59,10 +138,15 @@ export default {
       previewNotReady: true,
       firstCode: undefined,
       compilationCounter: 0,
+      mode: 0,
     };
   },
 
   methods: {
+    changeMode(mode: number) {
+      this.mode = mode;
+    },
+
     compile(code: string) {
       console.log("liascript: compile");
 
