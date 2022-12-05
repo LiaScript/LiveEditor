013c71ad713e872fb16ae4a11368833173a71dcc
André Dietrich <andredietrich@web.de>
2022-12-05 14:20:32 +0100

    Upgrade to LiaScript Version 0.10.25

---
+++ javascript.8e1487ba.js
@@ -0,0 +1,8 @@
+!function(){function e(e,n,t,i){Object.defineProperty(e,n,{get:t,set:i,enumerable:!0,configurable:!0})}var n=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire4f35;n.register("dimP8",(function(t,i){e(t.exports,"conf",(function(){return o})),e(t.exports,"language",(function(){return s}));
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+var a=n("7UIX8"),o=a.conf,s={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:a.language.operators,symbols:a.language.symbols,escapes:a.language.escapes,digits:a.language.digits,octaldigits:a.language.octaldigits,binarydigits:a.language.binarydigits,hexdigits:a.language.hexdigits,regexpctl:a.language.regexpctl,regexpesc:a.language.regexpesc,tokenizer:a.language.tokenizer}}))}();
